8ac4ab07bc8f96db7fc8807395a39118d33f3182
Mani kandan <mani22be@gmail.com>
2022-03-03 17:01:07 +0530

    api

--- script.js
+++ script.js
@@ -20,11 +20,12 @@ var mail = document.getElementById("mail");
 var cell = document.getElementById("cell");
 var mailText = document.getElementById("mailText");
 var containerr = document.getElementById("containerr");
-var mailer = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
-mailText.style.visibility ="hidden";
-nameText.style.visibility ="hidden";
-cellText.style.visibility ="hidden";
-containerr.style.maxHeight ="530px";
+var mailer =
+  /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+mailText.style.visibility = "hidden";
+nameText.style.visibility = "hidden";
+cellText.style.visibility = "hidden";
+containerr.style.maxHeight = "530px";
 
 /* function validateForm() {
 var a = document.forms["formss"]["fname"].value;
@@ -34,14 +35,46 @@ return false;
 }
 } */
 
+/* function callApi(){
+
+  fetch("https://api.mycaptain.co.in/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `mutation($formType:String,$firstName:String,$email:String,$mobileNumber:Int){
+      createLeadFromForms(lead:{
+          formType: $formType,
+          firstName: $firstName,
+          email: $email, 
+          mobileNumber: $mobileNumber})
+          {id}}`,
+      variables: {
+        formType: "pm_dm_pro",
+        firstName: names.value,
+        email: mail.value,
+        mobileNumber: cell.value
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data,"data here");
+    });
+  
+} */
+
 nextBtnFirst.addEventListener("click", function (event) {
-  if(names.value.length == 0 && mail.value.length == 0 && cell.value.length == 0){
-    nameText.style.visibility ="visible";
-    mailText.style.visibility ="visible";
-    cellText.style.visibility ="visible";
+  if (
+    names.value.length == 0 &&
+    mail.value.length == 0 &&
+    cell.value.length == 0
+  ) {
+    nameText.style.visibility = "visible";
+    mailText.style.visibility = "visible";
+    cellText.style.visibility = "visible";
   }
   if (names.value.length == 0) {
-    nameText.style.visibility ="visible";
+    nameText.style.visibility = "visible";
     event.preventDefault();
     names.focus();
     return false;
@@ -53,19 +86,68 @@ nextBtnFirst.addEventListener("click", function (event) {
   }
   if (mailer.test(mail.value) != true) {
     event.preventDefault();
-    mailText.style.visibility ="visible";
+    mailText.style.visibility = "visible";
     mail.focus();
     return false;
   }
   if (cell.value.length == 0) {
-    cellText.style.visibility ="visible";
+    cellText.style.visibility = "visible";
     event.preventDefault();
     cell.focus();
     return false;
-  }
-  
-  else {
-    
+  } else {
+    alert(cell.value, "loggggg");
+
+
+   /*  fetch("https://api.mycaptain.co.in/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `mutation($formType:String,$full_name:String,$email:String,$mobileNumber:String!){
+        createLeadFromForms(lead:{
+            formType: $formType,
+            full_name: $full_name,
+            email: $email, 
+            mobileNumber: $mobileNumber})
+            {id}}`,
+        variables: {
+          formType: "pm_dm_pro",
+          full_name: names.value,
+          email: mail.value,
+          mobileNumber: cell.value
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.createLeadFromForms.,"data here");
+      }); */
+
+ /*      updateLeadFromForms(id: ID, lead: {rating: 3, cityId: 1, yearOfStudy: "2012", stateId: 1, reason: "Valid reason", department: "CSE", currentProfile: "professional", interestedInJobAssurance: true, anyOtherCourseCompleted: true, myCaptainCourseCompleted: true}){
+        id
+        } */
+
+    fetch("https://api.mycaptain.co.in/graphql", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `mutation($id:Int!,$rating:Int,$reason:String){
+        updateLeadFromForms(id: $ID,lead:{
+            rating: $rating, 
+            reason: $reason})
+            {id}}`,
+        variables: {
+          id: parseInt("1837"),
+          rating: names.value,
+          reason: mail.value,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.createLeadFromForms,"data here");
+      });
+
     event.preventDefault();
     slidePage.style.marginLeft = "-25%";
     headPage.style.marginLeft = "-25%";
@@ -83,8 +165,7 @@ nextBtnSec.addEventListener("click", function (event) {
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
   current += 1;
-  containerr.style.maxHeight ="680px";
-
+  containerr.style.maxHeight = "680px";
 });
 nextBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
@@ -94,8 +175,7 @@ nextBtnThird.addEventListener("click", function (event) {
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
   current += 1;
-  containerr.style.maxHeight ="680px";
-
+  containerr.style.maxHeight = "680px";
 });
 submitBtn.addEventListener("click", function () {
   bullet[current - 1].classList.add("active");
