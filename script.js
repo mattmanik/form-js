8534072dda903dff5f433713b8de0a638acef5ff
Mani kandan <mani22be@gmail.com>
2022-03-03 23:11:47 +0530

    api integrated

--- script.js
+++ script.js
@@ -12,56 +12,29 @@ const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
 const progresser = document.querySelectorAll(".container .progress-bar");
 const inputName = document.querySelectorAll(".nameF");
-/* const nameF = document.querySelectorAll(".nameF");
-let x = document.forms["formss"]["fname"].value; */
 let current = 1;
 var names = document.getElementById("names");
 var mail = document.getElementById("mail");
 var cell = document.getElementById("cell");
 var mailText = document.getElementById("mailText");
 var containerr = document.getElementById("containerr");
+var profession = document.getElementById("profession");
+var branch = document.getElementById("branch");
+var yearofg = document.getElementById("yearofg");
+var textarea = document.getElementById("textarea");
+var ifJob = document.getElementById("ifJob");
+var ifDig = document.getElementById("ifDig");
+var rating = document.getElementById("rating");
 var mailer =
   /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
 mailText.style.visibility = "hidden";
 nameText.style.visibility = "hidden";
 cellText.style.visibility = "hidden";
 containerr.style.maxHeight = "530px";
+const objId = {};
 
-/* function validateForm() {
-var a = document.forms["formss"]["fname"].value;
-if (a == "") {
-alert("Please Fill All Required Field");
-return false;
-}
-} */
+var booleanObj = {};
 
-/* function callApi(){
-
-  fetch("https://api.mycaptain.co.in/graphql", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query: `mutation($formType:String,$firstName:String,$email:String,$mobileNumber:Int){
-      createLeadFromForms(lead:{
-          formType: $formType,
-          firstName: $firstName,
-          email: $email, 
-          mobileNumber: $mobileNumber})
-          {id}}`,
-      variables: {
-        formType: "pm_dm_pro",
-        firstName: names.value,
-        email: mail.value,
-        mobileNumber: cell.value
-      },
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data,"data here");
-    });
-  
-} */
 
 nextBtnFirst.addEventListener("click", function (event) {
   if (
@@ -95,24 +68,23 @@ nextBtnFirst.addEventListener("click", function (event) {
     event.preventDefault();
     cell.focus();
     return false;
-  } else {
-    alert(cell.value, "loggggg");
-
-
-   /*  fetch("https://api.mycaptain.co.in/graphql", {
+  }
+  
+  else {
+    fetch("https://api.mycaptain.co.in/graphql", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        query: `mutation($formType:String,$full_name:String,$email:String,$mobileNumber:String!){
+        query: `mutation($formType:String,$firstName:String,$email:String,$mobileNumber:String!){
         createLeadFromForms(lead:{
             formType: $formType,
-            full_name: $full_name,
+            firstName: $firstName,
             email: $email, 
             mobileNumber: $mobileNumber})
             {id}}`,
         variables: {
           formType: "pm_dm_pro",
-          full_name: names.value,
+          firstName: names.value,
           email: mail.value,
           mobileNumber: cell.value
         },
@@ -120,32 +92,8 @@ nextBtnFirst.addEventListener("click", function (event) {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.createLeadFromForms.,"data here");
-      }); */
-
- /*      updateLeadFromForms(id: ID, lead: {rating: 3, cityId: 1, yearOfStudy: "2012", stateId: 1, reason: "Valid reason", department: "CSE", currentProfile: "professional", interestedInJobAssurance: true, anyOtherCourseCompleted: true, myCaptainCourseCompleted: true}){
-        id
-        } */
-
-    fetch("https://api.mycaptain.co.in/graphql", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        query: `mutation($id:Int!,$rating:Int,$reason:String){
-        updateLeadFromForms(id: $ID,lead:{
-            rating: $rating, 
-            reason: $reason})
-            {id}}`,
-        variables: {
-          id: parseInt("1837"),
-          rating: names.value,
-          reason: mail.value,
-        },
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.createLeadFromForms,"data here");
+     objId.ids = data.data.createLeadFromForms.id;
+        console.log(data,"data here");
       });
 
     event.preventDefault();
@@ -155,9 +103,37 @@ nextBtnFirst.addEventListener("click", function (event) {
     progressCheck[current - 1].classList.add("active");
     progressText[current - 1].classList.add("active");
     current += 1;
+    console.log(objId,'objId');
   }
 });
+
+
 nextBtnSec.addEventListener("click", function (event) {
+  
+  fetch("https://api.mycaptain.co.in/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `mutation($id:Int!,$currentProfile:String,$department:String,$yearOfStudy:String){
+      updateLeadFromForms(id: $id,lead:{
+        currentProfile: $ currentProfile, 
+        department: $department,
+          yearOfStudy:$yearOfStudy
+        })
+          {id}}`,
+      variables: {
+        id: parseInt(objId.ids),
+        currentProfile: profession.value,
+        department:branch.value,
+        yearOfStudy: yearofg.value,
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data,"data from second ");
+    })
+
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
   headPage.style.marginLeft = "-50%";
@@ -167,7 +143,37 @@ nextBtnSec.addEventListener("click", function (event) {
   current += 1;
   containerr.style.maxHeight = "680px";
 });
+
+
 nextBtnThird.addEventListener("click", function (event) {
+
+  fetch("https://api.mycaptain.co.in/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `mutation($id:Int!,$reason:String,$interestedInJobAssurance:Boolean,$anyOtherCourseCompleted:Boolean,$rating:Int){
+      updateLeadFromForms(id: $id,lead:{
+         reason: $reason, 
+         interestedInJobAssurance: $interestedInJobAssurance,
+          anyOtherCourseCompleted:$anyOtherCourseCompleted,
+          rating:$rating
+        })
+          {id}}`,
+      variables: {
+        id: parseInt(objId.ids),
+        reason: textarea.value,
+        interestedInJobAssurance: ifJob.value.includes("Job") ? true : false,
+        anyOtherCourseCompleted: ifDig.value.includes("Yes") ? true : false,
+        rating:parseInt(rating.value)
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data,"data from third ");
+    })
+
+    
   event.preventDefault();
   slidePage.style.marginLeft = "-75%";
   headPage.style.marginLeft = "-75%";
@@ -176,16 +182,6 @@ nextBtnThird.addEventListener("click", function (event) {
   progressText[current - 1].classList.add("active");
   current += 1;
   containerr.style.maxHeight = "680px";
-});
-submitBtn.addEventListener("click", function () {
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-  setTimeout(function () {
-    alert("Your Form Successfully Signed up");
-    location.reload();
-  }, 800);
 });
 
 prevBtnSec.addEventListener("click", function (event) {
